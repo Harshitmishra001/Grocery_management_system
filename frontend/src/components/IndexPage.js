fdfe7cd28c2d215a8afa41acccd4eb207f4c4909
Alankriti-Mittal <alankritiaug26@gmail.com>
2025-03-23 10:30:18 +0530

    Front

---
+++ frontend/src/components/IndexPage.js
@@ -0,0 +1,13 @@
+// src/components/IndexPage.js
+import React from 'react';
+
+function IndexPage() {
+  return (
+    <div>
+      <h1>Welcome to Index Page</h1>
+      <p>You have successfully logged in!</p>
+    </div>
+  );
+}
+
+export default IndexPage;
